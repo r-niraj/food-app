7d518bc55c7b0b673ad218c285dcd5e641fffe9a
rajnish <krajnish114@gmail.com>
2025-12-07 11:01:54 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -80,7 +80,8 @@ function MainRoutes() {
 --------------------------------*/
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("./service-worker.js") 
+    .register("./service-worker.js")
     .then(() => console.log("Service Worker registered"))
     .catch((err) => console.log("SW registration failed:", err));
 }
+
